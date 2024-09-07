0a4577bf20085250dbc90b35ea9665d34f2bf787
Jelladotexe <135444856+Jelladotexe@users.noreply.github.com>
2024-09-07 20:05:50 +0700

    finish signin

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ window.addEventListener('scroll', () => {
     cloud2.style.top = value * -0.5 + 'px';
     
 });
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link'); 
+const registerLink = document.querySelector('.register-link'); 
+const btnPopup = document.querySelector('.btnLogin-popup'); 
+const iconClose = document.querySelector('.icon-close'); 
+
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
